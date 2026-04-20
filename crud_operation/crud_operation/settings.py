6ba4a398b2643c9c07285e298fcfc26bde41805b
diwakar chaudhary <boyroyal4853@gmail.com>
2026-04-21 00:33:42 +0530

    add

--- crud_operation/crud_operation/settings.py
+++ crud_operation/crud_operation/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'crud_operation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'student_crud/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
