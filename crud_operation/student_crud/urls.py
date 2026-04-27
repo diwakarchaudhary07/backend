2d0ef6d465bb85e560759b88d259176518851d4b
diwakar chaudhary <boyroyal4853@gmail.com>
2026-04-27 23:23:29 +0530

    add

--- crud_operation/student_crud/urls.py
+++ crud_operation/student_crud/urls.py
@@ -4,7 +4,7 @@ from .views import student_info, add_student, edit_student, delete_student
 
 
 urlpatterns = [
-    path('students', student_info, name='student_info'),  # 🔥 same name
+    path('students', student_info, name='student_info'),
     path('add/', add_student, name='add_student'),
     path('edit/<int:id>/', edit_student, name='edit_student'),
     path('delete/<int:id>/', delete_student, name='delete_student'),
