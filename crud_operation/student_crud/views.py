18131d08e39be5bace340abffff61aa600ff1900
diwakar chaudhary <boyroyal4853@gmail.com>
2026-04-28 22:00:08 +0530

    add

--- crud_operation/student_crud/views.py
+++ crud_operation/student_crud/views.py
@@ -6,39 +6,39 @@ from .forms import StudentForm
 # ✅ READ (List)
 def student_info(request):
     students = Student.objects.all()
-    return render(request, 'student_list.html', {'students': students})
+    return render(request, 'other.html', {'students': students})
 
 
 # ✅ CREATE
-def add_student(request):
-    if request.method == "POST":
-        form = StudentForm(request.POST, request.FILES)  # 🔥 important
-        if form.is_valid():
-            form.save()
-            return redirect('student_list')
-    else:
-        form = StudentForm()
+# def add_student(request):
+#     if request.method == "POST":
+#         form = StudentForm(request.POST, request.FILES)  # 🔥 important
+#         if form.is_valid():
+#             form.save()
+#             return redirect('student_list')
+#     else:
+#         form = StudentForm()
 
-    return render(request, 'student_form.html', {'form': form})
-
-
-# ✅ UPDATE
-def edit_student(request, id):
-    student = get_object_or_404(Student, id=id)
-
-    if request.method == "POST":
-        form = StudentForm(request.POST, request.FILES, instance=student)  # 🔥 important
-        if form.is_valid():
-            form.save()
-            return redirect('student_list')
-    else:
-        form = StudentForm(instance=student)
-
-    return render(request, 'student_form.html', {'form': form})
+#     return render(request, 'student_form.html', {'form': form})
 
 
-# ✅ DELETE
-def delete_student(request, id):
-    student = get_object_or_404(Student, id=id)
-    student.delete()
-    return redirect('student_list')
+# # ✅ UPDATE
+# def edit_student(request, id):
+#     student = get_object_or_404(Student, id=id)
+
+#     if request.method == "POST":
+#         form = StudentForm(request.POST, request.FILES, instance=student)  # 🔥 important
+#         if form.is_valid():
+#             form.save()
+#             return redirect('student_list')
+#     else:
+#         form = StudentForm(instance=student)
+
+#     return render(request, 'student_form.html', {'form': form})
+
+
+# # ✅ DELETE
+# def delete_student(request, id):
+#     student = get_object_or_404(Student, id=id)
+#     student.delete()
+#     return redirect('student_list')
